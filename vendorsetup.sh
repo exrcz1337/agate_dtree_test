d0b8c276669c212e5567811a0d302da214b04c1c
exerczz1 <exrcz513@gmail.com>
2026-09-08 22:36:20 +0600

    agate: Clean up proprietary files for pure HyperOS 1 extraction
    * Drop proprietary-firmware.txt and remove add_firmware_proprietary_file
    * Unpin all C2 blobs by removing legacy MIUI hashes
    * Remove legacy MIUI proprietary vendor clone from vendorsetup.sh
    
    Signed-off-by: exerczz1 <exrcz513@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,4 @@
 git clone https://github.com/exrcz1337/kernel_xiaomi_mt6893 -b test-clang-18 kernel/xiaomi/mt6893
-git clone https://github.com/exrcz1337/proprietary_vendor_xiaomi_agate -b lineage-23.2 vendor/xiaomi/agate
 git clone https://github.com/xiaomi-mt6893-dev/hardware_xiaomi.git hardware/xiaomi -b lineage-23.0
 git clone https://github.com/xiaomi-mt6893-dev/hardware_mediatek hardware/mediatek
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
