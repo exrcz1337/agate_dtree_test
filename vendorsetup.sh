4372c047bb5fcda4d15e2e5bab198b9cda8b404b
exerczz1 <exrcz513@gmail.com>
2026-09-08 22:43:43 +0600

    vendorsetup.sh: Switch hardware_xiaomi to lineage-23.2
    Signed-off-by: exerczz1 <exrcz513@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/exrcz1337/kernel_xiaomi_mt6893 -b test-clang-18 kernel/xiaomi/mt6893
-git clone https://github.com/xiaomi-mt6893-dev/hardware_xiaomi.git hardware/xiaomi -b lineage-23.0
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi -b lineage-23.2
 git clone https://github.com/xiaomi-mt6893-dev/hardware_mediatek hardware/mediatek
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
 git clone https://github.com/xiaomi-mediatek-devs/android_vendor_mediatek_ims vendor/mediatek/ims
